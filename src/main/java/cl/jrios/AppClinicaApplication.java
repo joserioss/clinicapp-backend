69bf6a174741f5d874709d3be2d4258d77ee7e74
Jose Rios <lithium@hotmail.es>
2020-10-10 12:54:52 -0300

    Inicio proyecto clinica, y entidad paciente basica backend

---
+++ src/main/java/cl/jrios/AppClinicaApplication.java
@@ -0,0 +1,13 @@
+package cl.jrios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppClinicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppClinicaApplication.class, args);
+	}
+
+}
