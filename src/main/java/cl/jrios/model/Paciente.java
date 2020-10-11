40a9fbb247550c79d3a772811bf85f86e3ff9a9f
Jose Rios <lithium@hotmail.es>
2020-10-11 12:50:23 -0300

    Rest controller paciente

--- src/main/java/cl/jrios/model/Paciente.java
+++ src/main/java/cl/jrios/model/Paciente.java
@@ -6,6 +6,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "paciente")
@@ -15,21 +17,27 @@ public class Paciente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idPaciente;
 
+	@Size(min = 3, message = "Nombres debe tener minimo 3 caracteres")
 	@Column(name = "nombres", nullable = false, length = 70)
 	private String nombres;
 
+	@Size(min = 3, message = "Apellidos debe tener minimo 3 caracteres")
 	@Column(name = "apellidos", nullable = false, length = 70)
 	private String apellidos;
 
+	@Size(min = 8, max = 8, message = "DNI debe tener 8 caracteres")
 	@Column(name = "dni", nullable = false, length = 8)
 	private String dni;
 
+	@Size(min = 3, max = 150, message = "Dirección debe tener minimo 3 caracteres")
 	@Column(name = "direccion", nullable = true, length = 150)
 	private String direccion;
 
+	@Size(min = 9, max = 9, message = "Telefono debe tener 9 caracteres")
 	@Column(name = "telefono", nullable = true, length = 9)
 	private String telefono;
 
+	@Email
 	@Column(name = "email", nullable = true, length = 55)
 	private String email;
 
